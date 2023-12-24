35567f6d9f0a56e6703c36a6405566237076d696
jovanoski05 <jovanoski.ivan12345@gmail.com>
2023-12-24 18:34:58 +0100

    Created index controller, index view and updated to show on public index

--- public/index.php
+++ public/index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "Hello World";
+include "../controllers/index.php";
---
+++ views/index.view.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ivan's Blog</title>
+</head>
+<body>
+    <h1>This is Ivan's Blog</h1>
+<body>
+</html>
